c7e3cdfe502a655d202e7ba1573b586a4e3d4040
Shafi Masoumi <masoumi2017@icloud.com>
2020-02-20 19:22:14 -0800

    secret file and restricted middleware created MVP done

--- api/server.js
+++ api/server.js
@@ -1,18 +1,40 @@
-const express =  require('express');
-const helmet =  require('helmet');
+const express = require('express');
+const helmet = require('helmet');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
+const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
-    res.send('server is up running')
+  res.send("server is live");
+});
+
+server.get('/token', (req, res) => {
+
+  const payload = {
+    subject: 'thisuser',
+    userid: 'shafimasoumi',
+    favoriteColor: 'blue'
+  };
+
+  const secret = 'letsdoit';
+  const options = {
+    expiresIn: '1d'
+  };
+
+  const token = jwt.sign(payload, secret, options);
+
+  res.json(token);
 })
 
 module.exports = server;
